0aa826a477e80440d3a9d415bd5ba7836218cc52
charliemulder <charliemulder@users.noreply.github.com>
2023-11-09 06:16:07 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,58 +1,42 @@
 // Create web server
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var session = require('express-session');
+// Import modules
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Create express server
+const app = express();
+
+// Set port
+const port = 3000;
 
 // Set view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// Set bodyParser
-app.use(bodyParser.urlencoded({ extended: false }));
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Set session
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
+// Set body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set path static
-app.use('/public', express.static('public'));
-
-// Set port
-app.listen(3000);
-
-// Connect database
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    port: '3306',
-    database: 'comment'
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/comment', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-connection.connect();
+// Import routes
+const indexRoute = require('./routes/index.route');
+const commentRoute = require('./routes/comment.route');
 
-// Set route
-app.get('/', function (req, res) {
-    res.render('index');
+// Use routes
+app.use('/', indexRoute);
+app.use('/comment', commentRoute);
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
-
-app.get('/comment', function (req, res) {
-    var sql = 'SELECT * FROM comment';
-    connection.query(sql, function (err, result) {
-        if (err) throw err;
-        res.render('comment', { comment: result });
-    });
-});
-
-app.post('/comment', function (req, res) {
-    var comment = { name: req.body.name, comment: req.body.comment };
-    var sql = 'INSERT INTO comment SET ?';
-    connection.query(sql, comment, function (err, result) {
-        if (err) throw err;
-        res.redirect('/
